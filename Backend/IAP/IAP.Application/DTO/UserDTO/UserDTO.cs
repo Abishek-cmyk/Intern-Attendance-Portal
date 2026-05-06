c503b8fe49ecca53eac941a679a88fb3ad831ac4
Abishek-cmyk <rockstarabishek101@gmail.com>
2026-05-06 12:27:26 +0530

    Added DTO

---
+++ Backend/IAP/IAP.Application/DTO/UserDTO/UserDTO.cs
@@ -0,0 +1,25 @@
+﻿using IAP.Domain.Entity;
+
+namespace IAP.Application.DTOs.User
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+
+        public int CompanyId { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public UserRole Role { get; set; }
+
+        public UserStatus Status { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
