41477870d82b539a664c208d773d872f77c083e5
inzamamulhaq01 <yasminbegam700@gmail.com>
2026-05-07 16:42:25 +0530

    Updated UserController,Repo,Services

--- Backend/IAP/IAP.Application/Services/UserService.cs
+++ Backend/IAP/IAP.Application/Services/UserService.cs
@@ -1,4 +1,5 @@
-﻿using IAP.Domain.Interface;
+﻿using IAP.Domain.Entity;
+using IAP.Domain.Interface;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,6 +12,54 @@ namespace IAP.Application.Services
         public UserService(IUserRepository UserRepo)
         {
             _userRepo = UserRepo;
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsers()
+        {
+            return await _userRepo.GetAllAsync();
+        }
+
+        public async Task<User?> GetUserById(int id)
+        {
+            return await _userRepo.GetUserByIdAsync(id);
+        }
+
+        public async Task<bool> CreateUserAsync(User NewUser)
+        {
+            var isExist = await _userRepo.IsUserExistByEmailAsync(NewUser.Email);
+            if (isExist)
+                return false;
+
+            var Password = NewUser.PasswordHash;
+            NewUser.PasswordHash = BCrypt.Net.BCrypt.HashPassword(Password);
+            NewUser.CreatedAt = DateTime.UtcNow;
+
+            await _userRepo.CreateUserAsync(NewUser);
+            return true;
+
+        }
+
+
+        // Update User
+        public async Task<bool> UpdateUserAsync(int id, User user)
+        {
+            user.UpdatedAt = DateTime.UtcNow;
+
+            return await _userRepo.UpdateUserByIdAsync(id, user);
+        }
+
+
+        // Delete User
+        public async Task<bool> DeleteUserAsync(int id)
+        {
+            var existingUser = await _userRepo.GetUserByIdAsync(id);
+
+            if (existingUser == null)
+                return false;
+
+            await _userRepo.DeleteUserByIdAsync(id);
+
+            return true;
         }
     }
 }
