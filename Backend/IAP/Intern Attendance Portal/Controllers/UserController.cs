68443c86de9ebf5ee23a85b190c545f35dfa9ab4
inzamamulhaq01 <yasminbegam700@gmail.com>
2026-05-07 17:18:23 +0530

    Updated UserController

--- Backend/IAP/Intern Attendance Portal/Controllers/UserController.cs
+++ Backend/IAP/Intern Attendance Portal/Controllers/UserController.cs
@@ -26,9 +26,34 @@ namespace Intern_Attendance_Portal.Controllers
         public async Task<ActionResult<IEnumerable<UserDTO>>> GetAllUsers()
         {
             var users = await _userService.GetAllUsers();
-            var usersDto = _mapper.Map<UserDTO>(users);
+            var usersDto = _mapper.Map<List<UserDTO>>(users);
             return Ok(usersDto);
         }
+
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<UserDTO>> GetUserById(int id)
+        {
+            var user = await _userService.GetUserById(id);
+            var userDto = _mapper.Map<UserDTO>(user);
+            return Ok(userDto);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateUser(CreateUserDTO NewUser)
+        {
+            User user = _mapper.Map<User>(NewUser);
+            bool isCreated = await _userService.CreateUserAsync(user);
+            if (isCreated == false)
+                return Conflict("User email already exists!");
+            return Ok("User created successfully...");
+        }
+
+        //[HttpPut]
+        //public async Task<ActionResult> UpdateUser()
+
+
 
 
     }
